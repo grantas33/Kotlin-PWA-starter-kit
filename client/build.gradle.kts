d0084f11b3a8808532cda2fa9f1016cfc961cceb
Grantas Gadliauskas <grantas.gadliauskas@nfq.lt>
2021-06-14 09:12:55 +0300

    bump component lib version

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.0-pre.204-kotlin-1.5.0")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.204-kotlin-1.5.0")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.204-kotlin-1.5.0")
-    implementation("com.github.grantas33:kotlin-pwa-components:v0.1.0")
+    implementation("com.github.grantas33:kotlin-pwa-components:v0.1.1")
     implementation(npm("react-loader-spinner", "3.1.14"))
 }
 
